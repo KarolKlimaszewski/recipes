c5686e68bbd0b75effe4897794fad3a00a4c5547
Karol Klimaszewski <karol.klimaszewski1997@gmail.com>
2018-09-21 17:33:51 +0200

    Fighting with Firebase

--- src/components/recipes.jsx
+++ src/components/recipes.jsx
@@ -11,12 +11,13 @@ export class Recipes extends React.Component {
         this.state = {
             recipes: [],
             loaded: false,
+            form: {},
             title: "",
             ingredients: "",
             photo: "",
             recipeSteps: "",
-            category: "",
-            addRecipeDisplay: "none"
+            category: "red",
+            addRecipeDisplay: "flex"
         }
     }
 
@@ -42,7 +43,7 @@ export class Recipes extends React.Component {
     handleAddRecipe = () => {
         if(this.state.addRecipeDisplay === "none"){
             this.setState({
-                addRecipeDisplay: "block"
+                addRecipeDisplay: "flex"
             })
         }
         else{
@@ -51,6 +52,48 @@ export class Recipes extends React.Component {
             })
         }
     }
+
+    //FORM
+
+    handleTitleChange = (event) => {
+        this.setState({
+            title: event.target.value
+        })
+    }
+
+    handleIngredientsChange = (event) => {
+        this.setState({
+            ingredients: event.target.value
+        })
+    }
+
+
+    handlePhotoUrlChange = (event) => {
+        this.setState({
+            photo: event.target.value
+        })
+    }
+
+
+    handleRecipeStepsChange = (event) => {
+        this.setState({
+            recipeSteps: event.target.value
+        })
+    }
+
+    handleSubmit = (event) => {
+        event.preventDefault();
+        let recipesRef = firebase.database().ref("recipes");
+        console.log(recipesRef);
+        recipesRef.push({
+                ingredients: this.state.ingredients,
+                photo: "this.state.photo,",
+                RecipeSteps: "this.state.RecipeSteps",
+                title: "this.state.title",
+                category: "red"
+            });
+    }
+
 
     render() {
         if (this.state.loaded) {
@@ -81,6 +124,10 @@ export class Recipes extends React.Component {
                             {recipeSteps}
                         </ul>
                     </div>
+                    <div className="recipe__row-edit">
+                        <button className="recipe__edit">Edit</button>
+                        <button className="recipe__delete">Delete</button>
+                    </div>
                 </div>
 
             })
@@ -91,12 +138,31 @@ export class Recipes extends React.Component {
                         {recipes}
                         <button className="recipe__add" onClick={this.handleAddRecipe}>+</button>
                         <form className="recipe__form" style={{display: this.state.addRecipeDisplay}}>
+                            <p className="recipe__form-description">
+                                Your recipe title:
+                            </p>
                             <input className={"recipe__form-input"} type="text" placeholder={"Title..."}
                                    value={this.state.title}
                                    onChange={this.handleTitleChange}/>
+                            <p className="recipe__form-description">
+                                Ingredients:
+                            </p>
                             <input className={"recipe__form-input"} type="text" placeholder={"ingredients..."}
                                    value={this.state.ingredients}
                                    onChange={this.handleIngredientsChange}/>
+                            <p className="recipe__form-description">
+                                Photo Url:
+                            </p>
+                            <input className={"recipe__form-input"} type="text" placeholder={"Your photo URL..."}
+                                   value={this.state.photo}
+                                   onChange={this.handlePhotoUrlChange}/>
+                            <p className="recipe__form-description">
+                                Recipe steps:
+                            </p>
+                            <input className={"recipe__form-input"} type="text" placeholder={"Recipe steps..."}
+                                   value={this.state.recipeSteps}
+                                   onChange={this.handleRecipeStepsChange}/>
+
                             <button type={"submit"} className={"addUser__submit"} onClick={this.handleSubmit}>
                                 Submit
                             </button>
