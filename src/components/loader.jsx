387ed3680aea33284e981317aa34e8502a613456
Karol Klimaszewski <karol.klimaszewski1997@gmail.com>
2018-10-04 15:50:57 +0200

    changed loader

--- src/components/loader.jsx
+++ src/components/loader.jsx
@@ -3,7 +3,23 @@ import React from "react";
 export class Loader extends React.Component {
     render() {
         return <div className={"loader__box"}>
-                <div className={"loader"} />
+            <h1 className={"loader__title"}>Cooking in progress..</h1>
+            <div id="cooking">
+                <div className="bubble"></div>
+                <div className="bubble"></div>
+                <div className="bubble"></div>
+                <div className="bubble"></div>
+                <div className="bubble"></div>
+                <div id="area">
+                    <div id="sides">
+                        <div id="pan"></div>
+                        <div id="handle"></div>
+                    </div>
+                    <div id="pancake">
+                        <div id="pastry"></div>
+                    </div>
+                </div>
+            </div>
             </div>
     }
 }
